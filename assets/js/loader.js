3feb5c76fc7ce2383be9fdc32b7382a720f2ae00
Paulo Alvarez <paulo.manuel.alvarez@gmail.com>
2024-11-17 17:40:08 -0300

    Modifico seccion 3d segun figma

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -2,5 +2,5 @@ window.addEventListener("load", function () {
     setTimeout(function () {
         const loader = document.getElementById("loader");
         loader.style.display = "none"; // Oculta el loader después del tiempo de espera
-    }, 5000); // Tiempo en milisegundos (2000 ms = 2 segundos)
+    }, 1); // Tiempo en milisegundos (2000 ms = 2 segundos)
 });
