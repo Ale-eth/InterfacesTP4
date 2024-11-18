b21dc9f3238739dfc6d6e5a7be917755e559a8b8
Josefina Belaunzaran <josefinabelaunzaran0106@gmail.com>
2024-11-18 18:25:14 -0300

    modifico loader con numeros

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,22 +1,22 @@
 window.addEventListener("load", function () {
-    const stars = document.querySelectorAll('.star');
-    let currentStar = 0;
+    const blocks = document.querySelectorAll('.block');
+    let currentBlock = 0;
 
-    // Función para encender las estrellas secuencialmente
-    function lightUpStar() {
-        if (currentStar < stars.length) {
-            stars[currentStar].classList.add('on'); // Agrega la clase 'on' para encender la estrella
-            currentStar++;
+    // Función para iluminar los bloques secuencialmente
+    function lightUpBlock() {
+        if (currentBlock < blocks.length) {
+            blocks[currentBlock].classList.add('on'); // Ilumina el bloque
+            currentBlock++;
         }
     }
 
-    // Encender una estrella cada segundo
-    const starInterval = setInterval(lightUpStar, 1000);
+    // Iluminar un bloque cada segundo
+    const blockInterval = setInterval(lightUpBlock, 1000);
 
     // Ocultar el loader después de 5 segundos
     setTimeout(function () {
-        clearInterval(starInterval); // Detenemos la secuencia
+        clearInterval(blockInterval); // Detenemos la secuencia
         const loader = document.getElementById("loader");
         loader.style.display = "none"; // Ocultar el loader
-    }, 1);
+    }, 5500);
 });
